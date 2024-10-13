75e2666bd294e1a4262803b77fedfdd513b5de37
SetarehRooz <115781068+SetarehRooz@users.noreply.github.com>
2024-10-13 22:04:06 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,12 +7,12 @@ function preload() {
 }
 
 function setup() {
-  let canvas = createCanvas(img.width, img.height);
+  createCanvas(img.width, img.height);
   canvas.parent('sketch-holder'); // Attach canvas to the div with ID sketch-holder
   noLoop();
   img.loadPixels();
 
-  // Extract pixels and store them as point coordinates
+// Extract pixels and store them as point coordinates
   for (let x = 0; x < img.width; x++) {
     for (let y = 0; y < img.height; y++) {
       let index = (x + y * img.width) * 4;
@@ -23,7 +23,7 @@ function setup() {
       // Check brightness to define if the point will be added
       let brightness = (r + g + b) / 3;
       if (brightness < 200) { // Adjust threshold to control density
-        points.push(createVector(x, y));
+        points.push({ pos: createVector(x, y), color: color(r, g, b) }); // Store position and color
       }
     }
   }
@@ -37,9 +37,9 @@ function draw() {
       if (currentPoint >= points.length) break;
 
       let pt = points[currentPoint];
-      stroke(0);
+      stroke(pt.color); // Set stroke color to the point's color
       strokeWeight(1);
-      point(pt.x, pt.y);
+      point(pt.pos.x, pt.pos.y); // Draw the point
       currentPoint++;
     }
   }
